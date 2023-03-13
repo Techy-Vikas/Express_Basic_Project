316165c62abd86b7eb497498d19a293831260acd
Vikas Chauhan <vikasonline98765@gmail.com>
2023-03-13 21:55:24 +0530

    add few more controller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const homeController = require('../controllers/home_controller')
 const router = express.Router();
 
 router.get('/',homeController.home)
-router.get('/action',homeController.actionName)
+router.use('/users',require('./users'))
+router.use('/posts',require('./posts'))
+//for any further routes, access from here
+//router.use('/routeName',require('./routeFile'));
 
 module.exports = router;
