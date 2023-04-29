b9ae6f3f649e766f0988d91798f873fa7e20a16e
Vikas Chauhan <vikasonline98765@gmail.com>
2023-04-29 19:28:49 +0530

    install jwt

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,54 @@
-module.exports.index = (req,res) => {
+const Comment = require("../../../models/comment")
+const Post = require("../../../models/post")
+
+module.exports.index = async (req,res) => {
+
+    let posts = await Post.find({}).sort('-createdAt')
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user',
+                }
+            })
+
     return res.json(200,{
         message : 'Lists of Posts',
-        posts: []
+        posts: posts
     })
+}
+
+module.exports.destroy = async (req, res) => {
+    try {
+        let post = await Post.findById(req.params.id)
+
+        // if (post.user == req.user.id) {
+            // post.remove();
+            await Post.deleteOne(post)
+            await Comment.deleteMany({ post: req.params.id });
+            // if(req.xhr){
+            //     return res.status(200).json({
+            //         data: {
+            //             post_id: req.params.id
+            //         },
+            //         message: "Post Deleted"
+            //     })
+            // }
+            // return res.redirect('back');
+            return res.json(200,{
+                message : "Post and Associated Comments Deleted Successfully!"
+            })
+
+        // }
+        // else {
+        //     return res.redirect('back');
+        // }
+    } catch (err) {
+        console.log('err', err);
+        res.json(500,{
+            message : 'Internal Server Error',
+        })
+
+    }
+
 }
---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,26 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+//sign in and create a session for the user
+module.exports.createSession = async (req,res) => {
+    try{
+    let user = await User.findOne({email : req.body.email});
+    if(!user || user.password != req.body.password){
+        return res.status(422).json({
+            message : "Invalid UserName or password"
+        })
+    }
+
+    return res.status(200).json({
+        message : 'Sign in successfully, here is your token, please keep it safe!',
+        data: {
+            token : jwt.sign(user.toJSON(),'codeial',{expiresIn : '10000'}),
+        }
+    })
+    }catch(err){
+        console.log('error',err);
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+}
